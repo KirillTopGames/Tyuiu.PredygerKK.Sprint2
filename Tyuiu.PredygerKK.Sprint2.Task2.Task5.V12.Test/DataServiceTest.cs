006f8801e71ab19dfa05989809f408633164050a
Кирилл Предыгер <kirillpredyger6@gmail.com>
2024-10-16 10:38:35 +0500

    Task5

---
+++ Tyuiu.PredygerKK.Sprint2.Task2.Task5.V12.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using Tyuiu.PredygerKK.Sprint2.Task2.Task5.V12.Lib;
+
+namespace Tyuiu.PredygerKK.Sprint2.Task2.Task5.V12.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            DataService ds = new DataService();
+            int g, m, n;
+            g = 1997;
+            m = 1;
+            n = 1;
+
+            Assert.AreEqual("1996.12.31", ds.FindDateOfPreviousDay(g, m, n));
+        }
+    }
+}
